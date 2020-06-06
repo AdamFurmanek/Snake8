941276a7086011a3a10862e12e80347b7ff1231d
AdamFurmanek <62468000+AdamFurmanek@users.noreply.github.com>
2020-06-07 01:53:27 +0200

    Skalowanie na podstawie mapy
    - Zawartość panelu "rozgrywka" skaluje się dodatkowo w zależności od wielkości mapy

--- src/Okno.java
+++ src/Okno.java
@@ -3,22 +3,22 @@ import javax.swing.JFrame;
 public class Okno extends JFrame{
 	
 	char[][] mapa;
-	int wysokoscMapy=4;
-	int szerokoscMapy=4;
+	int wysokoscMapy=50;
+	int szerokoscMapy=100;
 	
 	private Okno()  throws Exception{
 		
 		super("Snake8");
+		setSize(1286, 743);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-		setSize(1920, 1080);
 		setLocationRelativeTo(null);
 		
 		mapa = new char[wysokoscMapy][szerokoscMapy];
 		
 		Rozgrywka rozgrywka = new Rozgrywka(this);
 		add(rozgrywka);
-		
+
 	}
 	
 	public static void main(String[] args)  throws Exception{
--- src/Rozgrywka.java
+++ src/Rozgrywka.java
@@ -10,31 +10,33 @@ import javax.swing.JPanel;
 public class Rozgrywka extends JPanel {
 
 	Okno okno;
-	double skalaWysokosc, skalaSzerokosc;
+	double skalaWysokoscOkno, skalaSzerokoscOkno;
+	double skalaWysokoscMapa, skalaSzerokoscMapa;
 
 	public Rozgrywka(Okno okno) throws Exception {
 		this.okno = okno;
 		this.addComponentListener(new ComponentAdapter() {
 			public void componentResized(ComponentEvent e) {
-				skalaWysokosc = (double) okno.getHeight() / 1080;
-				skalaSzerokosc = (double) okno.getWidth() / 1920;
-				System.out.println(skalaSzerokosc);
-				System.out.println(skalaWysokosc);
+				skalaWysokoscOkno = (double) okno.getHeight() / 1080;
+				skalaSzerokoscOkno = (double) okno.getWidth() / 1920;
 			}
 		});
+		skalaWysokoscMapa=(double)880/(okno.wysokoscMapy*200);
+		skalaSzerokoscMapa=(double)1720/(okno.szerokoscMapy*200);
 	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
+		
 		Graphics2D g2d = (Graphics2D) g.create();
 
 		AffineTransform at = new AffineTransform();
-		at.scale(skalaSzerokosc, skalaSzerokosc);
+		at.scale(skalaSzerokoscOkno*skalaSzerokoscMapa, skalaWysokoscOkno*skalaWysokoscMapa);
 		g2d.setTransform(at);
-
+		
 		for (int i = 0; i < okno.szerokoscMapy; i++) {
 			for (int j = 0; j < okno.wysokoscMapy; j++) {
-				new ImageIcon("images/0.png").paintIcon(this, g2d, i * 200, j * 200);
+				new ImageIcon("images/0.png").paintIcon(this, g2d, (int)(94/(skalaSzerokoscMapa) + i * 200), (int)(77/(skalaWysokoscMapa) + j * 200));
 			}
 		}
 	}
